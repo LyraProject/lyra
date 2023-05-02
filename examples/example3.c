18d3affb43d037c3ad893f2881dcb8c015efe18a
Lion Kortlepel <development@kortlepel.com>
2023-05-02 15:07:29 +0200

    add lyra_tsque implementation + example

---
+++ examples/example3.c
@@ -0,0 +1,57 @@
+#include "lyra_tsque.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <threads.h>
+
+/// This example demonstrates the use of `lyra_tsque`.
+/// Whatever you enter is echoed from different threads back to the
+/// user.
+/// Watch the CPU usage while using this example - even though it spawns
+/// 10 threads, all of them are waiting (for input or waiting on the queue).
+
+int thread_main(void* data) {
+    lyra_tsque* q = (lyra_tsque*)data;
+
+    while (true) {
+        char buf[10];
+        lyra_tsque_wait_and_pop(q, buf);
+        printf("thread %lu got: %s\n", thrd_current(), buf);
+    }
+
+    return 0;
+}
+
+int main(void) {
+    const lyra_tsque_init_args args = {
+        .count = 10,
+        .element_size = sizeof(const char[10]),
+        .alloc = malloc,
+        .dealloc = free,
+    };
+    lyra_tsque* q = lyra_tsque_init(&args);
+    if (!q) {
+        return 1;
+    }
+
+    thrd_t th[10];
+    for (size_t i = 0; i < 10; ++i) {
+        thrd_create(&th[i], thread_main, q);
+    }
+
+    char buf[10];
+
+    printf("enter some text, the thread will print it word by word.\n"
+           "watch the cpu usage - the thread is not spinning.\n");
+
+    while (true) {
+        memset(buf, 0, sizeof(buf));
+        scanf("%9s", buf);
+        lyra_tsque_push(q, buf);
+    }
+
+    for (size_t i = 0; i < 10; ++i) {
+        thrd_join(th[i], NULL);
+    }
+    lyra_tsque_deinit(q);
+}
